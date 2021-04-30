4203a6556edc20c32ef6451cf30c3ae894369ce3
baishkahi <baishakhi.d@makonissoft.com>
2021-04-30 13:51:07 +0530

    updated admin to support custom user model

--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
-    PermissionsMixin
+    PermissionsMixin, AbstractUser
 
 
 class UserManager(BaseUserManager):
---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,41 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+    def setUp(self) -> None:
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email="admin@example.com",
+            password="pass#123"
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email="test@example.com",
+            password="pass#123",
+            name="Test user"
+        )
+
+    def test_users_listed(self):
+        """Test that user are listed on user page"""
+        url = reverse('admin:core_user_changelist')
+        resp = self.client.get(url)
+        self.assertContains(resp, self.user.name)
+        self.assertContains(resp, self.user.email)
+
+    def test_user_change_page(self):
+        """"Test that the user edit page works"""
+        url = reverse("admin:core_user_change", args=[self.user.id])
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test that the create user page works"""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+
+
