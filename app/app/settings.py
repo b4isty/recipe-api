83907b583f50b25a92f058feec3474da373a669e
baishkahi <baishakhi.d@makonissoft.com>
2021-05-03 22:51:56 +0530

    Added endpoint for creating users

--- app/app/settings.py
+++ app/app/settings.py
@@ -35,8 +35,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
 
     'core',
+    'user',
 ]
 
 MIDDLEWARE = [
--- app/core/models.py
+++ app/core/models.py
@@ -6,22 +6,20 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
-        """Creates and saves a new User"""
+        """Create and saves a new user"""
         if not email:
-            raise ValueError('Users must have an email address')
+            raise ValueError("User must have an email address")
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
-
         return user
 
     def create_superuser(self, email, password):
-        """ Create and save a new super user"""
+        """Creates and saves a new superuser"""
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
-
         return user
 
 
