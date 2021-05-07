3eac197af3e6b5adedab1d40813b37664c8de184
baishkahi <baishakhi.d@makonissoft.com>
2021-05-07 16:52:52 +0530

    Added image field to recipe model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,3 +1,4 @@
+from unittest.mock import patch
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from core import models
@@ -67,3 +68,12 @@ class ModelTests(TestCase):
             price=5.00
         )
         self.assertEqual(str(recipe), recipe.title)
+
+    @patch('uuid.uuid4')
+    def test_recipe_file_name_uuid(self, mock_uuid):
+        """Test that image is saved in the correct location"""
+        uuid = "test_uuid"
+        mock_uuid.return_value = uuid
+        file_path = models.recipe_image_file_path(None, "myimage.jpg")
+        exp_path = f'uploads/recipe/{uuid}.jpg'
+        self.assertEqual(file_path, exp_path)
